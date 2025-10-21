e34ed80fcd3e35a951b2103caedd2da1215f7f7e
VarunSuvarna <vs2152830@gmail.com>
2025-10-21 15:29:55 +0000

    fixed the sidebar issues.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,9 +23,9 @@ export function Sidebar({ isOpen = false }: SidebarProps) {
   return (
     <aside
       className={cn(
-        "fixed lg:sticky top-0 left-0 h-screen z-40 flex flex-col border-r border-[#15b9b7]/20 bg-[#001324] transition-all duration-300",
+        "fixed lg:sticky top-0 left-0 h-screen z-40 flex flex-col border-r border-[#15b9b7]/20 bg-[#001324] transition-all duration-300 group",
         // Always show icons on desktop, full sidebar when open
-        isOpen ? "w-64" : "w-16"
+        isOpen ? "w-64" : "w-16 hover:w-64"
       )}
     >
       <nav className="flex-1 flex flex-col gap-1 p-2 mt-16">
@@ -44,11 +44,12 @@ export function Sidebar({ isOpen = false }: SidebarProps) {
               title={!isOpen ? item.name : undefined}
             >
               <item.icon className="w-5 h-5 flex-shrink-0" />
-              {isOpen && (
-                <span className="text-sm font-medium whitespace-nowrap">
-                  {item.name}
-                </span>
-              )}
+              <span className={cn(
+                "text-sm font-medium whitespace-nowrap transition-opacity duration-300",
+                isOpen ? "opacity-100" : "opacity-0 group-hover:opacity-100"
+              )}>
+                {item.name}
+              </span>
             </Link>
           )
         })}
